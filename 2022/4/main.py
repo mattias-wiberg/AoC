3533b1bf126ec870110133eea59cd43cc228ae78
Mattias Wiberg <klutten99@gmail.com>
2022-12-04 14:30:20 +0100

    Day 4 2022

--- 2022/4/main.py
+++ 2022/4/main.py
@@ -7,10 +7,30 @@ def read_input(path):
         return f.read()
 
 def part1(data):
-    pass
+    def contain(a, b): # a contains b
+        return a[0]<=b[0] and a[1]>=b[1]
+
+    count = 0
+    for row in data.split("\n"):
+        a,b = row.split(",")
+        a = list(map(int, a.split("-")))
+        b = list(map(int, b.split("-")))
+        if contain(a, b) or contain(b, a):
+            count += 1
+    return count
 
 def part2(data):
-    pass
+    def contain(a, b): # a contains b
+        return (a[0]<=b[0] and b[0]<=a[1]) or (a[0]<=b[1] and b[1]<=a[1])
+
+    count = 0
+    for row in data.split("\n"):
+        a,b = row.split(",")
+        a = list(map(int, a.split("-")))
+        b = list(map(int, b.split("-")))
+        if contain(a, b) or contain(b, a):
+            count += 1
+    return count
 
 if __name__ == "__main__":
     data = read_input(os.path.dirname(os.path.abspath(__file__)))
